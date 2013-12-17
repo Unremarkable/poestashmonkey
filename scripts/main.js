d5baf13d0843968da473681ebd284fd41f7c2f9a
Andrew Hausch <andrew.hausch@gmail.com>
2013-12-17 15:44:22 -0800

    yet another..

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
-alert($);
-alert(require);
-
-require([
-    "data/mods.js",
-    "data/baseWeapons.js",
-    "jungle.js",
-], function() {
-    console.log(baseWeapons);
-    console.log(modData);
-    ready();
-});
+$.getScript("http://requirejs.org/docs/release/2.1.9/minified/require.js")
+.done(function() {
+	require([
+		"data/mods.js",
+		"data/baseWeapons.js",
+		"jungle.js",
+	], function() {
+		console.log(baseWeapons);
+		console.log(modData);
+		ready();
+	});
+};
