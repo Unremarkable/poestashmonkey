c92bdbd59d6d84e051e3fdd50113acc9d527e837
Andrew Hausch <andrew.hausch@gmail.com>
2013-12-17 15:47:33 -0800

    explicit url

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 $.getScript("http://requirejs.org/docs/release/2.1.9/minified/require.js")
 .done(function() {
 	require([
-		"./data/mods.js",
-		"./data/baseWeapons.js",
-		"./jungle.js",
+		"https://raw.github.com/Unremarkable/poestashmonkey/master/scripts/data/mods.js",
+		"https://raw.github.com/Unremarkable/poestashmonkey/master/scripts/data/baseWeapons.js",
+		"https://raw.github.com/Unremarkable/poestashmonkey/master/scripts/jungle.js",
 	], function() {
 		console.log(baseWeapons);
 		console.log(modData);
