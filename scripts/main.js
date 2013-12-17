286fc14c596c36e303834cb02e333b0dfd3d44dc
Andrew Hausch <andrew.hausch@gmail.com>
2013-12-17 15:46:24 -0800

    mussing with main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 $.getScript("http://requirejs.org/docs/release/2.1.9/minified/require.js")
 .done(function() {
 	require([
-		"data/mods.js",
-		"data/baseWeapons.js",
-		"jungle.js",
+		"./data/mods.js",
+		"./data/baseWeapons.js",
+		"./jungle.js",
 	], function() {
 		console.log(baseWeapons);
 		console.log(modData);
