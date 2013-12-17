7960058d37983b534b453f1115d5c8398a60a3e4
Andrew Hausch <andrew.hausch@gmail.com>
2013-12-17 15:57:04 -0800

    Added greasemonkey script

---
+++ StashScript.userscript.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        StashScript
+// @namespace   www.pathofexile.com
+// @description Script to see more things on the PoE Stash
+// @include     http://www.pathofexile.com/*
+// @require     http://code.jquery.com/jquery-1.10.2.min.js
+// @require     https://raw.github.com/Unremarkable/poestashmonkey/master/scripts/main.js
+// @version     1
+// @grant       none
+// ==/UserScript==
