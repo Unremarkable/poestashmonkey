837b5ad51f842f7e4bb9ddfe5a28b0d543b060e7
Zelenata <firecomet77@gmail.com>
2015-11-25 00:14:24 -0800

    Updating the user script to use https.

--- StashScript.userscript.js
+++ StashScript.userscript.js
@@ -2,13 +2,13 @@
 // @name        StashScript
 // @namespace   www.pathofexile.com
 // @description Script to see more things on the PoE Stash
-// @include     http://www.pathofexile.com/character-window/*
+// @include     https://www.pathofexile.com/character-window/*
 // @require     http://code.jquery.com/jquery-1.10.2.min.js
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-var BASE_URL = "https://rawgithub.com/Unremarkable/poestashmonkey/master"
+var BASE_URL = "https://rawgithub.com/Unremarkable/poestashmonkey/master";
 
 $.getScript(BASE_URL+"/scripts/main.js", function() {
     main(BASE_URL);
