283fa1f5e94673a3c9aa9020ac221bf6cf41c4a7
Zelenata <firecomet77@gmail.com>
2016-03-28 19:46:03 -0700

    Updating currency ratios to chaos thanks to Drau's currency knowledge.

--- scripts/currency.js
+++ scripts/currency.js
@@ -23,25 +23,25 @@ function fetchCurrencyConversionTable(){
 
 // data from PoE trade as of 2015-12-23
 var currencyToChaosMap = {
-		"Orb of Augmentation" : 1.0/50.0,
-		"Orb of Transmutation" : 1.0/40.0,
+		"Orb of Augmentation" : 1.0/20.0,
+		"Orb of Transmutation" : 1.0/30.0,
 		"Armourer's Scrap" : 1.0/20.0,
-		"Chromatic Orb" : 1.0/13.0,
-		"Orb of Alteration" : 1.0/11.0,
+		"Chromatic Orb" : 1.0/8.0,
+		"Orb of Alteration" : 1.0/10.0,
 		"Blacksmith's Whetstone" : 1/10.0,
-		"Jeweller's Orb" : 1.0/6.0,
+		"Jeweller's Orb" : 1.0/5.0,
 		"Orb of Chance" : 1.0/5.0,
 		"Orb of Alchemy" : 1.0/4.0,
 		"Cartographer's Chisel" : 1.0/3.0,
-		"Orb of Fusing" : 1.0/2.0,
+		"Orb of Fusing" : 4.0/5.0,
 		"Orb of Scouring" : 2.0/3.0,
 		"Orb of Chaos" : 1.0,
 		"Orb of Regret" : 3.0/2.0,
-		"Regal Orb" : 3.0/2.0,
+		"Regal Orb" : 5.0/4.0,
 		"Vaal Orb" : 2.0,
 		"Blessed Orb" : 2.0,
 		"Gemcutter's Prism" : 3.0,
 		"Divine Orb" : 25.0,
-		"Exalted Orb" : 60.0,
+		"Exalted Orb" : 61.0,
 		"Mirror of Kalendra" : 18000.0
 };
